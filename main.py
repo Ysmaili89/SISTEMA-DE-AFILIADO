1a276928019d5281b2cc3aaa965ce3c167eee653
ysmaili Joran <joransantana88@mail.com>
2025-08-02 09:25:38 -0400

    Migración: crear tabla producto y actualizar modelos

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-# main.py
-from app import create_app
+from app import create_app, create_initial_data
 
 app = create_app()
+
+# Crear tablas y datos iniciales si no existen (solo en Render o producción)
+create_initial_data(app)

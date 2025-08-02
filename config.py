5f11ce2afccd73ad977f9b66ee325dc9887df51b
ysmaili Joran <joransantana88@mail.com>
2025-08-02 12:07:36 -0400

    Migración: crear tabla producto y actualizar modelos

--- config.py
+++ config.py
@@ -1,24 +1,16 @@
 import os
 
 class Config:
-    # Clave secreta de seguridad para sesiones, tokens, etc.
+    # Clave secreta para seguridad de sesiones, etc.
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'una_clave_secreta_por_defecto_solo_para_desarrollo_local'
     
-    # Obtener la URL original
+    # Obtener la URL de la base de datos desde variable de entorno
     uri = os.getenv('DATABASE_URL') or \
         'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.db')
 
-    # Reemplazar el esquema si es PostgreSQL
+    # Si usas PostgreSQL, ajustar esquema para psycopg 3 y SQLAlchemy 2.x
     if uri.startswith("postgresql://"):
         uri = uri.replace("postgresql://", "postgresql+psycopg://", 1)
 
     SQLALCHEMY_DATABASE_URI = uri
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # Otras posibles configuraciones
-    # MAIL_SERVER = 'smtp.googlemail.com'
-    # MAIL_PORT = 587
-    # MAIL_USE_TLS = True
-    # MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    # MAIL_PASSWORD = os.environ.get('EMAIL_PASS')

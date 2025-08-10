709bc6771220a05877baf9af96d1af8068e77707
ysmaili Joran <joransantana88@mail.com>
2025-08-10 15:10:00 -0400

    Migración: crear tabla producto y actualizar modelos

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,12 +1,8 @@
-# routes/__init__.py
-
 from flask import Blueprint
 
 # Crear un blueprint para las rutas
-bp = Blueprint('routes', __name__)
+bp = Blueprint('rutas', __name__)
 
 # Importar los módulos con las rutas para registrarlas en el blueprint
-from . import afiliados_routes
-from . import productos_routes
-
-# Aquí podrías también importar más módulos de rutas si tienes
+from . import afiliados_routes  # noqa: F401
+from . import productos_routes  # noqa: F401
